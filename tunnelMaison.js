8aa1178c208c031ee6d50f4fd1cf465d343af18e
gwladyslaboss <40230806+gwladyslaboss@users.noreply.github.com>
2018-06-13 10:27:14 +0200

    J'ai rajouté des questions dans le parcours

--- tunnelMaison.js
+++ tunnelMaison.js
@@ -1,19 +1,54 @@
-window.tunnelMaison = [
-    {
-        "question": "Envisagez-vous de réorganiser l'espace de votre appartement ?",
-        "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
-        "answers": [
-            {
-                "label": "Oui, j’ai les plans en tête",
-                "next": "1",
-            }, {
-                "label": "Oui, mais je n’ai pas d’idées précises de ce que je veux faire",
-                "next": "1",
-            }, {
-                "label": "Non",
-                "next": "2",
-            }
-        ],
-        "relatedQuestion" : "Pourquoi c'est important ?"
-    }
-]
+window.tunnelMaison = [{
+    "question": "Envisagez-vous d'agrandir l'espace de votre logement ?",
+    "details": "Surélévation, extension latérale, véranda, terrasse...",
+    "answers": [{
+        "label": "Oui, surface finale au sol > 150m²",
+        "next": "1"
+    }, {
+        "label": "Oui, surface finale au sol < 150m²",
+        "next": "1"
+    }, {
+        "label": "Non",
+        "next": "1"
+    }],
+    "relatedQuestion": "Comment la calculer ?"
+}, {
+    "question": "Envisagez-vous de réorganiser l'espace de votre maison ?",
+    "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+    "answers": [{
+        "label": "Oui, j’ai les plans en tête",
+        "next": "2"
+    }, {
+        "label": "Oui, mais je n’ai pas d’idées précises de ce que je veux faire",
+        "next": "2"
+    }, {
+        "label": "Non",
+        "next": "3"
+    }],
+    "relatedQuestion": "Qui est habilité à expertiser une structure ?"
+}, {
+    "question": "Un expert a-t-il bien identifié qu'aucune structure porteuse n'est impliquée dans votre projet ?",
+    "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+    "answers": [{
+        "label": "Oui",
+        "next": "3"
+    }, {
+        "label": "Non",
+        "next": "3"
+    }],
+    "relatedQuestion": "Qui est habilité à expertiser une structure ?"
+}, {
+    "question": "Comptez-vous refaire les installations ou les revêtements dans votre maison ?",
+    "details": "Plomberie, électricité, maçonnerie, menuiserie, parquets…",
+    "answers": [{
+        "label": "Oui, je sais ce que je veux faire",
+        "next": ""
+    }, {
+        "label": "Oui, mais je ne sais pas quoi refaire",
+        "next": ""
+    }, {
+        "label": "Non",
+        "next": ""
+    }],
+    "relatedQuestion": "Comment savoir ?"
+}]
