d0a0c4a7f8cb70bb3ad204ba52a69179e179cec7
Antoine-emmanuel <antoine.sansegundo@gmail.com>
2018-06-13 16:02:40 +0200

    Update diagnosticsMaison.js

--- diagnosticsMaison.js
+++ diagnosticsMaison.js
@@ -1,6 +1,11 @@
 window.diagnosticsMaison = {
-    "[0,0,0]": {
-        "text": ``,
+    "[2,0,1,0]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel pour la conception de votre projet est une vrai nécessité. 
+Dans votre cas, il est recommandé de passer par un architecte ou à minima par un bureau d'études structures pour vous assurer :
+- qu'aucune structure porteuse n'est concernée dans votre projet
+- ou, le cas échéant, définir les reprises de structure nécessaire. 
+Un architecte pourra également vous aider à optimiser votre projet (installations à refaire, identification de problèmes que vous n'auriez pas vus...).
+`,
         "mandatoryPros": [
         ],
         "requiredPros": [
@@ -18,7 +23,7 @@ window.diagnosticsMaison = {
         ],
         "links": [
             {
-                "label": "Se renseigner sur la démarche à suivre auprès du syndic de copro",
+                "label": "Et si vous êtes en copropriété : les précautions à prendre",
                 "href": "#"
             }
         ]
