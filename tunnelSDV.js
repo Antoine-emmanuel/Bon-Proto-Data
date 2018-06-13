2501e885baa90eb53e1786a7b0755bbda658ddff
gwladyslaboss <40230806+gwladyslaboss@users.noreply.github.com>
2018-06-13 12:51:45 +0200

    Questions et parcours

--- tunnelSDV.js
+++ tunnelSDV.js
@@ -1,19 +1,48 @@
 window.tunnelSDV = [
-    {
-        "question": "Envisagez-vous de réorganiser l'espace de votre appartement ?",
-        "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+  {
+        "question": "Votre logement est-il en copropriété ?",
+        "details": "",
         "answers": [
             {
-                "label": "Oui, j’ai les plans en tête",
-                "next": "1",
-            }, {
-                "label": "Oui, mais je n’ai pas d’idées précises de ce que je veux faire",
+                "label": "Oui",
                 "next": "1",
             }, {
                 "label": "Non",
-                "next": "2",
+                "next": "1",
             }
         ],
         "relatedQuestion" : "Pourquoi c'est important ?"
+    }, {
+        "question": "Envisagez-vous de réorganiser l'espace de votre cuisine ?",
+        "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+        "answers": [
+            {
+                "label": "Oui et j'ai les plans en tête",
+                "next": "2",
+            }, {
+                "label": "Oui, mais pas d'idées",
+                "next": "2",
+            }, {
+                "label": "Non",
+                "next": "3",
+            }
+        ],
+        "relatedQuestion" : ""
+    }, {
+         "question": "Comptez-vous refaire les installations ou les revêtements dans votre logement ?",
+        "details": "Plomberie, éléctricité, maçonnerie, menuiserie, parquets,…",
+        "answers": [
+            {
+                "label": "Oui, je sais ce que je veux faire",
+                "next": "",
+            }, {
+                "label": "Oui, mais je ne sais pas quoi refaire",
+                "next": "",
+            }, {
+                "label": "Non",
+                "next": "",
+            }
+        ],
+        "relatedQuestion" : "Comment savoir ?"
     }
 ]
