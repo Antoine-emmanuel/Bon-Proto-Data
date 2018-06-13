ca3e5d09ad7f2df4c116a91392e7ed1fab24f594
gwladyslaboss <40230806+gwladyslaboss@users.noreply.github.com>
2018-06-13 12:42:52 +0200

    Questions et parcours

--- tunnelCuisine.js
+++ tunnelCuisine.js
@@ -1,19 +1,73 @@
 window.tunnelCuisine = [
     {
-        "question": "Envisagez-vous de réorganiser l'espace de votre appartement ?",
-        "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+        "question": "Votre logement est-il en copropriété ?",
+        "details": "",
         "answers": [
             {
-                "label": "Oui, j’ai les plans en tête",
-                "next": "1",
-            }, {
-                "label": "Oui, mais je n’ai pas d’idées précises de ce que je veux faire",
+                "label": "Oui",
                 "next": "1",
             }, {
                 "label": "Non",
-                "next": "2",
+                "next": "1",
             }
         ],
         "relatedQuestion" : "Pourquoi c'est important ?"
-    }
-]
+    }, {
+        "question": "Envisagez-vous de réorganiser l'espace de votre cuisine ?",
+        "details": "Redistribution de l'espace, modification de cloisons, de fenêtres ou portes…",
+        "answers": [
+            {
+                "label": "Oui et j'ai les plans en tête",
+                "next": "2",
+            }, {
+                "label": "Oui, mais pas d'idées",
+                "next": "2",
+            }, {
+                "label": "Non",
+                "next": "3",
+            }
+        ],
+        "relatedQuestion" : ""
+    }, {
+        "question": "Un expert a-t-il bien identifié qu'aucune structure porteuse n'est impliquée dans votre projet ?",
+        "details": "",
+        "answers": [
+            {
+                "label": "Oui",
+                "next": "3",
+            }, {
+                "label": "Non",
+                "next": "3",
+            }
+        ],
+        "relatedQuestion" : "Qui est habilité à expertiser une structure ?"
+    }, {
+        "question": "Comptez-vous réagencer les meubles de votre cuisine ?",
+        "details": "Re-concevoir l'espace, créer un îlot, déplacer un point d'eau",
+        "answers": [
+            {
+                "label": "Oui",
+                "next": "4",
+            }, {
+                "label": "Non",
+                "next": "4",
+            }
+        ],
+        "relatedQuestion" : ""
+    }, {
+        "question": "Comptez-vous refaire les installations ou les revêtements dans votre cuisine ?",
+        "details": "Mise aux normes éléctriques, choix de l'éclairage ou des revêtements muraux...",
+        "answers": [
+            {
+                "label": "Oui, je sais ce que je veux faire",
+                "next": "",
+            }, {
+                "label": "Oui, mais je ne sais pas quoi faire",
+                "next": "",
+            }, {
+                "label": "Non",
+                "next": "",
+            }
+        ],
+        "relatedQuestion" : "Comment savoir ?"
+    }, {
