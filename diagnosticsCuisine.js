719445240ba3a71576eacd91a53008a9022caecd
gwladyslaboss <40230806+gwladyslaboss@users.noreply.github.com>
2018-06-14 15:12:15 +0200

    Update diagnosticsCuisine.js

--- diagnosticsCuisine.js
+++ diagnosticsCuisine.js
@@ -1,14 +1,13 @@
 window.diagnosticsCuisine = {
-    "[0,0,0]": {
-        "text": ``,
+    "[0,0,0,0,0]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel pour la conception de votre projet est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. Si c'est la cas,  votre projet devra être validé en assemblée générale des copropriétaires, avec les conclusions de l'architecte sur la faisabilité des travaux et les précautions à prendre, avant de lancer les travaux. 
+Il pourra vous aider à optimiser votre projet (installations à refaire, identification de problèmes que vous n'auriez pas vus...)`,
         "mandatoryPros": [
         ],
         "requiredPros": [
             {
                 "label": "Architecte",
-                "href": "#"
-            }, {
-                "label": "Bureau d'études structures",
                 "href": "#"
             }
         ],
@@ -18,9 +17,147 @@ window.diagnosticsCuisine = {
         ],
         "links": [
             {
-                "label": "Se renseigner sur la démarche à suivre auprès du syndic de copro",
+                "label": "Les précautions à prendre en copropriété",
                 "href": "#"
             }
         ]
+        }, "[1,0,0,0,0]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel pour la conception de votre projet est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. 
+Il pourra vous aider à optimiser votre projet (installations à refaire, identification de problèmes que vous n'auriez pas vus...)`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "",
+                "href" : "#"
+            }
+        ]
+    }, "[0,0,0,0,1]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. Si c'est la cas, votre projet devra être validé en assemblée générale des copropriétaires, avec les conclusions de l'architecte sur la faisabilité des travaux et les précautions à prendre, avant de lancer les travaux. 
+ Il vous aidera également à définir les installations ou équipements qu'il convient de refaire`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "Les précautions à prendre en copropriété",
+                "href" : "#"
+            }
+        ]
+    }, "[1,0,0,0,1]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. 
+ Il vous aidera également à définir les installations ou équipements qu'il convient de refaire`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "",
+                "href" : "#"
+            }
+        ]
+    }, "[0,0,0,0,2]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. Si c'est la cas, votre projet devra être validé en assemblée générale des copropriétaires, avec les conclusions de l'architecte sur la faisabilité des travaux et les précautions à prendre, avant de lancer les travaux. Il pourra également identifier si votre projet implique d'autres travaux qui vous auraient échappés.`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "Les précautions à prendre en copropriété",
+                "href" : "#"
+            }
+        ]
+    }, "[1,0,0,0,2]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. Il pourra également identifier si votre projet implique d'autres travaux qui vous auraient échappés.`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "",
+                "href" : "#"
+            }
+        ]
+    }, "[0,0,0,1,1]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. Si c'est la cas, votre projet devra être validé en assemblée générale des copropriétaires, avec les conclusions de l'architecte sur la faisabilité des travaux et les précautions à prendre, avant de lancer les travaux. 
+ Il vous aidera également à définir les installations ou équipements qu'il convient de refaire`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "Les précautions à prendre en copropriété",
+                "href" : "#"
+            }
+        ]
+    }, "[1,0,0,1,1]": {
+        "text": `Oui, pas de doute, vous faire accompagner par un professionnel est une vrai nécessité. 
+ Dans votre cas, il est recommandé de passer par un architecte pour vous assurer qu'aucune structure porteuse n'est concernée dans votre projet. 
+ Il vous aidera également à définir les installations ou équipements qu'il convient de refaire`,
+        "mandatoryPros": [],
+        "requiredPros": [
+            {
+                "label": "Architecte",
+                "href": "#"
+            }
+        ],
+        "recommendedPros": [],
+        "otherPros" : [],
+        "links" : [
+            {
+                "label" : "",
+                "href" : "#"
+            }
+        ]
+    },
+        
+        
+        
+        
+        
+        
     }
 }
